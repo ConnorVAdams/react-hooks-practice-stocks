b5216d8ca91f8db4be53d04b037dcb3e104d3805
Connor Adams <connorvadams@gmail.com>
2023-10-09 11:42:27 -0600

    Initial commit.

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,15 +1,27 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+  const [stocks, setStocks] = useState([])
+  const [sort, setsort] = useState({
+    sort: '',
+    filter: 'All'
+  })
+
+  useEffect(() => {
+    fetch('http://localhost:3001/stocks')
+    .then(resp => resp.json())
+    .then(fetchedStocks => setStocks(fetchedStocks))
+  })
+
   return (
     <div>
       <SearchBar />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={stocks}/>
         </div>
         <div className="col-4">
           <PortfolioContainer />
--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Stock from "./Stock";
 
-function StockContainer() {
+function StockContainer({ stocks }) {
+
+  const stocksToDisplay = stocks.map(stock => {
+    return <Stock key={stock.id} {...stock} />
+  })
+
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      {stocksToDisplay}
     </div>
   );
 }
